037682b39f79f5175dd9d3fc8b42d5f3f27dd9dd
somamurata <101445039+SomaMurata@users.noreply.github.com>
2022-04-01 16:46:10 +0900

    Create MemoEditScreen and MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import MemoLietScren from './src/screns/MemoListScren';
-// import MemoDetailScren from './src/screns/MemoDetailScren';
+
+// import MemoLietScren from './src/screns/MemoListScreen';
+// import MemoDetailScren from './src/screns/MemoDetailScreen';
+// import MemoEditScreen from './src/screns/MemoEditScreen';
+import MemoCreateScreen from './src/screns/MemoCreateScreen';
 
 // HTML的な記述
 export default function App() {
-  return <MemoLietScren />;
+  return (
+    <MemoCreateScreen />
+  );
 }
