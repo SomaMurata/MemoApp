dbe8a47a597b6ca402bbd94badca3216f48f53fb
somamurata <101445039+SomaMurata@users.noreply.github.com>
2022-04-02 22:25:49 +0900

    install ReactNvigation ets...

--- App.jsx
+++ App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import MemoLietScren from './src/screns/MemoListScreen';
 // import MemoDetailScren from './src/screns/MemoDetailScreen';
@@ -7,9 +9,15 @@ import MemoLietScren from './src/screns/MemoListScreen';
 // import LogInScreen from './src/screns/LogInScreen';
 // import SignUpscreen from './src/screns/SignUpScreen';
 
+const Stack = createStackNavigator();
+
 // HTML的な記述
 export default function App() {
   return (
-    <MemoLietScren />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MemoLietScren} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
