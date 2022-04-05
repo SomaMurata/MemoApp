4bc0c532c5df3e3ea7e2c3b5f911f635b06d1076
somamurata <101445039+SomaMurata@users.noreply.github.com>
2022-04-05 16:27:44 +0900

    Auto login and logout BUtton

--- src/screns/LogInScreen.jsx
+++ src/screns/LogInScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View,
   Text,
@@ -16,6 +16,18 @@ export default function LogInScreen(props) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  useEffect(() => {
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        navigation.reset({
+          index: 0,
+          routes: [{ name: 'MemoList' }],
+        });
+      }
+    });
+    return unsubscribe;
+  }, []);
+  // 上記[]でマウントされたときのみ読み込み
   function handlePress() {
     firebase
       .auth()
